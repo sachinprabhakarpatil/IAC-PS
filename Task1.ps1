48da1fe62c9c526cd291c5e5d93e462fe35a2198
sachinprabhakarpatil <54082173+sachinprabhakarpatil@users.noreply.github.com>
2019-08-14 02:00:02 +0530

    Add files via upload

---
+++ Task1.ps1
@@ -0,0 +1,42 @@
+﻿Get-PSDrive
+
+Get-Command -Noun Location
+
+Get-Help Set-Location -ShowWindow
+
+Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+
+Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced 
+
+Get-Command -Noun ItemProperty
+
+Get-Help Set-ItemProperty -ShowWindow
+
+Get-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name "DontPrettyPath"
+
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name "DontPrettyPath" -Value "1"
+
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name "DontPrettyPath" -Value "0"
+
+
+Get-Command -Noun  Item
+Get-Help New-Item -ShowWindow
+
+New-Item -ItemType Directory -Path C:\Demo
+
+New-Item -ItemType File -Path C:\Demo\test.txt
+
+Get-ItemProperty -Path C:\Demo\test.txt | Fl *
+
+
+Set-ItemProperty -Path C:\Demo\test.txt -Name "IsReadOnly" -Value "False"
+
+$env:TEMP
+
+
+
+
+
+
+
+
