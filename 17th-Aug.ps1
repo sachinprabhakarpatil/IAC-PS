0fb6fee8c85299f3d24dd5a5f4af97e2461fe8db
sachinprabhakarpatil <54082173+sachinprabhakarpatil@users.noreply.github.com>
2019-08-27 10:15:48 +0530

    Add files via upload

---
+++ 17th-Aug.ps1
@@ -0,0 +1,99 @@
+﻿Start-Transcript -Path "c:\Dinesh_Sir_Do_Not_ Delete_This_Folder\history.txt"
+Get-Service |Get-Member
+gsv|gm
+"rakesh"| gm
+"rakesh".Length
+"rakesh".Contains("k")
+"rakesh".Replace("s","$")
+"rakesh".Replace("s",1)
+"r1kesh".Replace(1,"a")
+"RAKESH".ToLower()
+"rakesh".Remove(0,2)
+$name=rakesh
+Write-Host "$name"
+Write-Host '$name'
+'This ny name "$name"'
+$myarr= @("dinesh",1,"patil")
+$myarr| gm
+Get-Member -InputObject $myarr
+Get-Member -InputObject $myarr
+$myarr.Length
+Get-PSProvider
+Get-Command -Noun PSProvider
+Get-ChildItem -Path c:\
+Get-Item -Path C:\Windows|fl *
+Get-ItemProperty -Path C:\Windows\*
+Get-ItemProperty -Path C:\Windows|fl *
+Get-Item Env:\
+Get-Item Env:\TEMP
+Get-ChildItem cert:\ -Recurse
+New-Item -Path c:\ -Name "demo" -ItemType "directory"
+New-Item -Path c:\demo2 -ItemType "directory"
+Get-ChildItem C:\Windows | Out-File -FilePath c:\demo\files.txt
+Get-ChildItem C:\Windows | Export-Csv  c:\demo\files.csv -Append
+$path=Read-Host 
+Get-ChildItem -Path $path
+Get-Help write-Host -Detailed
+Write-Host "give your path"
+$path=
+Get-ChildItem -Path $path
+
+
+echo "give your value1"
+[INT]$var1=Read-Host
+echo "give your value2"
+[INT]$var2=Read-Host
+if( $var1 -le $var2)
+{ echo "$var1 is less than $var2"
+}
+else
+{echo "$var1 is greater than $var2"
+}
+
+
+
+$service= Get-Service -Name BITS
+if( $service.Status -eq "Running")
+{ Write-Host "The service is running"
+}
+elseif( $service.Status -eq "Stopped")
+{Write-Host "The service is stopped"
+}
+else 
+{ Write-Host "The service is suspended"
+}
+
+Get-Help about_switch
+Update-Help
+
+
+
+$service= Get-Service -Name BITS
+Switch ($service.Status)
+{
+Stopped { Write-Host "The service is stopped"}
+Running {Write-Host "The service is Running"}
+Suspended { Write-Host "The service is Suspended"}
+}
+
+"11" -eq 11
+11 -eq "11"
+"dinesh" -match "in"
+"dinesh" -match "^d"
+"dinesh" -match "^d$"
+"inesh" -match "^d"
+"dinesh" -match "sh$"
+"dinesh" -match "^d.....$"
+"dinesh" -match "^d....h$"
+"dinesh" -match "d[in]nesh"
+"dinesh" -match "^d{5}$"
+"ddddd" -match "^d{5}$"
+"ddinesh" -match "^d{1}.....$"
+"dddd" -match "^d{5}$"
+"dddddd" -match "^d{5,}"
+"ddddddddd" -match "^d{5,7}$"
+
+for($i=0;$i -le 4;$i=$i+1)
+{ Write-Host "*"  
+}
+Get-help write-host -Examples
---
+++ 24th-Aug.ps1
@@ -0,0 +1,182 @@
+﻿# HASHTABLE
+$student=@{name="dinesh"
+             id=1} 
+
+$student
+$student.ContainsKey("name")
+$student.ContainsValue("dinesh")
+$student.GetHashCode()
+$student.Keys
+$student.Count
+$student.Values
+$student.Add("name2","sir") 
+$student.id=@(1,2,3)
+
+$array=@(1,2,3)
+$student.id=$array
+$array.Add(4)
+ 
+#To add object in array (arraylist)
+
+[System.Collections.ArrayList]$arraylist=@(1,2,3)
+$student.id=$arraylist
+$student.id
+$arraylist.Add(4)
+$arraylist.Remove(2)
+$arraylist[2]=5
+
+
+#PIPELINE
+
+Get-Service|
+
+Get-Help Stop-Service -ShowWindow
+Get-Service|gm
+Get-Service| Stop-Process
+Get-Help Stop-Process -ShowWindow
+
+Get-Service BITS|Stop-Process
+
+Get-Service|Stop-Process -WhatIf
+    
+Get-Process| Stop-Service -WhatIf
+Get-Help Stop-Service -ShowWindow
+
+Get-ChildItem C:\Windows\System32 | Stop-Process
+Get-ChildItem |gm
+Get-Help Stop-Process -ShowWindow
+
+Get-Service |gm
+Get-Service -DisplayName *WINDOWS*
+
+
+$myservices=Import-Csv .\POWERSHELL.CSV
+$myservices
+$myservices|Stop-Process
+$myservices|select sname,@{l="name";e={$_.sname}}|Stop-Process
+
+foreach
+
+
+# HASTABLE IN ARRAY
+$students=@(
+             @{name="dinesh"},
+             @{name="basant"},
+             @{name="prashant"}
+              )
+$students
+
+
+
+$students.Add("name","ritesh")
+
+
+#ARRAY IN HASHTABLE
+
+$studentdetails=@{name="dinesh"
+                  sub=@("math","science")
+                   }
+$studentdetails
+
+# HASHTABLE
+$student=@{name="dinesh"
+             id=1} 
+
+$student
+
+
+# HASTABLE IN ARRAY
+$students=@(
+             @{name="dinesh"},
+             @{name="basant"},
+             @{name="prashant"}
+              )
+
+$students
+
+#ARRAY IN HASHTABLE
+
+$studentdetails=@{name="dinesh"
+                  sub=@("math","science")
+                   }
+$studentdetails
+
+
+
+
+============================FOR EACH LOOP==========================
+
+$adusers=Import-Csv .\ADUSERS.csv
+$exusers=Import-Csv .\EXusers.csv
+
+
+ foreach( $aduser in $adusers)
+           {  $matched=$exusers| Where-Object {$_.email -eq $aduser.mail} | Measure-Object
+
+    if( $matched.Count -ge 1)
+               { 
+            $matchusers=$exusers| Where-Object {$_.email -eq $aduser.mail
+
+                          $outputuser=[pscustomobject]@{name=$aduser.name
+                                                         totalsize=$matchusers.totalsize }
+                }
+                $outputuser|Export-Csv C:\demo\match.csv -Append
+            }
+      else
+            { 
+              $aduser.name |Out-File c:\demo\notmatch.csv 
+            }
+
+           }
+
+
+Get-Process
+
+ $output=Get-Process |Where-Object {$_.ws -ge 100KB}|Select-Object ProcessName,ws
+ $output | gm
+ $MB = $output.ws/1024
+
+ Get-Process |Where-Object {$_.ws -ge 100KB}| select name,@{l="WS(MB)";e={$_.ws/1MB}}
+
+
+$text="dinesh"
+$reversetext = ""
+for ($i=$text.Length; $i -ge 0; $i--)
+
+ {
+
+ $reversetext = $reversetext+$text[$i]
+
+ 
+ }
+ 
+ $reversetext
+
+
+ Write-Host "give first num"
+ [int]$firstnum = Read-Host
+ Write-Host "give sec num"
+ [int]$secnum = Read-Host
+ Write-Host "give 1-add 2-sub num"
+ [int]$operation = Read-Host 
+ switch( $operation )
+ { 
+    1 {write-host "add is" ($firstnum + $secnum) } 
+    2 {Write-Host "sub is" ($firstnum - $secnum)}
+ }
+
+************************************************************
+ for *******
+     **
+
+for($i=5;$i -gt 0;$i=$i-1){
+
+Write-Host "" 
+
+for($j=$i;$j -gt 0;$j=$j-1){
+
+Write-Host "*" -NoNewline
+}
+
+}
+************************************************************
